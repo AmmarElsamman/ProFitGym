d3e19df3383610080389a59118754cb2cdd4d11f
AmmarElsamman <elsamman321@gmail.com>
2025-12-27 07:05:13 +0200

    Finished angular skeleton

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+  private http = inject(HttpClient)
+  private baseApiUrl = 'https://localhost:5001/api';
+  protected title = 'Pro Fit Gym';
+  protected clients = signal<any>([]);
+
+  async ngOnInit() {
+    this.clients.set(await this.getClients())
+  }
+
+  // * since we no longer using observables, we don't need to worry about subscribing and unsubscribing 
+  async getClients() {
+    try {
+      return lastValueFrom(this.http.get(this.baseApiUrl + '/clients/'));
+    } catch (error) {
+      console.error('There was an error!', error);
+      throw error;
+    }
+  }
+
 }
